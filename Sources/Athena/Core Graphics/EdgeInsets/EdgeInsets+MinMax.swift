3446447dca6f4dca62a7294a75d0b820514824e1
Samuel Franz <sam.franz@whcc.com>
2020-01-10 12:29:18 -0600

    Turns out I was not getting the max value correctly.

--- Sources/Athena/Core Graphics/EdgeInsets/EdgeInsets+MinMax.swift
+++ Sources/Athena/Core Graphics/EdgeInsets/EdgeInsets+MinMax.swift
@@ -5,12 +5,17 @@ import UIKit
 
 public extension UIEdgeInsets {
     
-    mutating func max(by: value: CGFloat) {
-        self = maxed(to: value)
+    mutating func max(by value: CGFloat) {
+        self = maxed(by: value)
     }
     
     func maxed(by value: CGFloat) -> UIEdgeInsets {
-        return UIEdgeInsets.init(max(top, value), max(left, value), max(bottom, value), max(right, value))
+        .init(
+            Swift.max(top, value),
+            Swift.max(left, value),
+            Swift.max(bottom, value),
+            Swift.max(right, value)
+        )
     }
     
 }
